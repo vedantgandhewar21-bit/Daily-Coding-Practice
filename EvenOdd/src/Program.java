ad33c1196a1185aca7a18103599b418a7c8c90ff
Vedant Gandhewar <vedantgandhewar21@gmail.com>
2026-05-19 00:43:33 +0530

    Q1

---
+++ EvenOdd/src/Program.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Program {
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int n= sc.nextInt();
+	System.out.println(n%2==0? "Even":"Odd");
+}
+}
